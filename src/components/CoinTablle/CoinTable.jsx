e905289bc967a90843f070a832f1818c7cb049ca
Shruti Verma <shrutive1009@gmail.com>
2024-08-13 15:48:32 +0530

    Added react query config

--- src/components/CoinTablle/CoinTable.jsx
+++ src/components/CoinTablle/CoinTable.jsx
@@ -1,4 +1,37 @@
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+
+import { fetchCoinData } from "../../services/fetchCoinData";
+
 function CoinTable() {
-  return <>Coin Table</>;
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isError, error } = useQuery(
+    ["coins", page],
+    () => fetchCoinData(page, "usd"),
+    {
+      retry: 2,
+      retryDelay: 1000,
+      cacheTime: 1000 * 60 * 2,
+    }
+  );
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <>
+      Coin Table <button onClick={() => setPage(page + 1)}>Click</button>
+      {page}
+    </>
+  );
 }
 export default CoinTable;
