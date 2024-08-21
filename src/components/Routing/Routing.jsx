8a4861ce4cca27277d56b5ab92882cdcf763f0bc
Shruti Verma <shrutive1009@gmail.com>
2024-08-21 23:49:33 +0530

    Added error boundary

--- src/components/Routing/Routing.jsx
+++ src/components/Routing/Routing.jsx
@@ -3,31 +3,34 @@ import { lazy, Suspense } from "react";
 import PageLoder from "../PageLoader/PageLoader";
 import MainLayout from "../../pages/Layout";
 import ContentLoader, { Facebook } from "react-content-loader";
+import CustomErrorBoundary from "../ErrorBoundary/CustomErrorBoundary";
 const Home = lazy(() => import("../../pages/Home"));
 const CoinDetailsPage = lazy(() => import("../../pages/CoinDetailsPage"));
 
 function Routing() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route
-          index
-          element={
-            <Suspense fallback={<PageLoder />}>
-              <Home />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/details/:coinId"
-          element={
-            <Suspense fallback={<PageLoder />}>
-              <CoinDetailsPage />
-            </Suspense>
-          }
-        />
-      </Route>
-    </Routes>
+    <CustomErrorBoundary>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route
+            index
+            element={
+              <Suspense fallback={<PageLoder />}>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/details/:coinId"
+            element={
+              <Suspense fallback={<PageLoder />}>
+                <CoinDetailsPage />
+              </Suspense>
+            }
+          />
+        </Route>
+      </Routes>
+    </CustomErrorBoundary>
   );
 }
 
